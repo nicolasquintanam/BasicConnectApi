770fea08d0611e99ebc9e13f3a7e3468e62ece73
Nicolás Quintana <nicolasquintanam@gmail.com>
2023-12-25 23:47:12 -0600

    implement confirm-email

--- src/Services/EmailConfirmationService/EmailConfirmationService.cs
+++ src/Services/EmailConfirmationService/EmailConfirmationService.cs
@@ -1,7 +1,16 @@
 namespace BasicConnectApi.Services;
 
+using BasicConnectApi.Data;
+
 public class EmailConfirmationService : IEmailConfirmationService
 {
+    private readonly ApplicationDbContext _dbContext;
+
+    public EmailConfirmationService(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
     public async Task SendConfirmationEmailAsync(string userEmail)
     {
         //TODO: implement email confirmation sending
@@ -9,7 +18,14 @@ public class EmailConfirmationService : IEmailConfirmationService
 
     public bool ConfirmEmail(string userEmail, string confirmationToken)
     {
-        //TODO: implement email confirmation
+        var user = _dbContext.User.FirstOrDefault(u => u.Email == userEmail);
+        if (user == null)
+            return false;
+
+        if (user.EmailConfirmationToken != confirmationToken)
+            return false;
+
+        user.IsEmailConfirmed = true;
         return true;
     }
 }
